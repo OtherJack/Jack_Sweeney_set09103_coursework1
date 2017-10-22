6aa03a274edef10add9d5e994f45a6fee897060e
OtherJack <jacksweeney@outlook.com>
2017-10-22 21:04:52 +0000

    bootstrap issue

---
+++ bootstrapTest.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+  return render_template('bootstrapTest.html'), 200
+
+if __name__ == "__main__":
+  
+  app.run(host="0.0.0.0", debug=True)
