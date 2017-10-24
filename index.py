7538e1422e7e107ecce6497acc1efe7d09e83ada
OtherJack <jacksweeney@outlook.com>
2017-10-24 18:18:47 +0000

    Whole day's work, Req, Resp, Redirects etc

--- index.py
+++ index.py
@@ -1,21 +1,72 @@
-from flask import Flask, render_template
+import ConfigParser
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-  return render_template('index.html'),200
+  return render_template('index.html'), 200
 
-#@app.route('/static-lcd/img')
-#def static_lcd_img():
- #start = '<img src="'
-  #url = url_for('static', filename='lcd.jpg')
-  #end = '">'
-  #return start+url+end, 200
+@app.route('/LCDSoundsystem')
+def lcd():
+  return render_template('LCD.html'), 200
+
+@app.route("/LCD")
+def relcd():
+  return redirect(url_for('lcd'))
+
+@app.route('/ArcadeFire')
+def af():
+  return render_template('AF.html'), 200
+
+@app.route('/AF')
+def reaf():
+  return redirect(url_for('af'))
+
+@app.route('/Radiohead')
+def rh():
+  return render_template('RH.html'), 200
+
+@app.route('/RH')
+def rerh():
+  return redirect(url_for('rh'))
+
+@app.route('/ArcticMonkeys')
+def am():
+  return render_template('AM.html'), 200
+
+@app.route('/AM')
+def ream():
+  return redirect(url_for('am'))
 
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('404.html'), 404
 
-if __name__ == "__main__":
-  
-  app.run(host="0.0.0.0", debug=True)
+@app.route('/config/')
+def config():
+  str = []
+  str.append('Debug:'+app.config['DEBUG'])
+  str.append('port:'+app.config['port'])
+  str.append('url:'+app.config['url'])
+  str.append('ip_address:'+app.config['ip_address'])
+  return '/t'.join(str)
+
+def init(app):
+    config = ConfigParser.ConfigParser()
+    try:
+        config_location = "etc/defaults.cfg"
+        config.read(config_location)
+
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print "Could not read configs from: ", config_location
+
+if __name__  == '__main__':
+    init(app)
+    app.run(
+        host=app.config['ip_address'],
+        port=int(app.config['port']))
+
